e000374738ef11dd79b439538af8e66ca40c059d
kipposhi <k.kabuhei725@gmail.com>
2022-03-10 00:17:28 +1100

    SQLQ$ fix

--- src/questions/SQLQuestion2.js
+++ src/questions/SQLQuestion2.js
@@ -13,7 +13,7 @@ import ReactDOM from 'react-dom';
 <div  style={{wordBreak: "break-word", marginTop: 10}}>
     SELECT player_id, CONCAT(first_name, ' ', last_name) 
     AS full_name FROM players INNER JOIN games<br/>
-    ON players.favourite_game = games.game_name 
+    ON players.favourite_game = games.game_id 
     WHERE games.game_name = 'SLOT';
 
 
